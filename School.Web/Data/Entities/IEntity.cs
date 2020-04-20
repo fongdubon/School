ce19379d7ab9b6f8eb31bf43e77573c71389dbe7
Carlos Alberto Marquez Silva <carlosalberto.marquezs@my.uvm.edu.mx>
2020-04-20 14:50:57 -0500

    Actualizacion

---
+++ School.Web/Data/Entities/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace School.Web.Data.Entities
+{
+    interface IEntity
+    {
+        int Id { get; set; }
+    }
+}
