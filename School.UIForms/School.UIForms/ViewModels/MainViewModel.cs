f902f072658f6c24f2187ca11e1629b818dcfbfe
Eduardo Fong Dubon <eduardofongdubon@hotmail.com>
2020-05-20 17:09:05 -0500

    Agregar modelo subject en Common, agregamos subjectsViewModel en UIForms, agregamos la vista de subjectspage, creamos el apicontroller de subjects

--- School.UIForms/School.UIForms/ViewModels/MainViewModel.cs
+++ School.UIForms/School.UIForms/ViewModels/MainViewModel.cs
@@ -3,6 +3,7 @@
 	public class MainViewModel
 	{
 		public LoginViewModel Login { get; set; }
+		public SubjectsViewModel Subjects { get; set; }
 		public MainViewModel()
 		{
 			this.Login = new LoginViewModel();
---
+++ School.UIForms/School.UIForms/ViewModels/SubjectsViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace School.UIForms.ViewModels
+{
+    using School.Common.Models;
+    using School.Common.Services;
+    using System;
+    using System.Collections.ObjectModel;
+
+    public class SubjectsViewModel
+    {
+        private ApiService apiService;
+        public ObservableCollection<Subject> Subjects { get; set; }
+
+        public SubjectsViewModel()
+        {
+            this.apiService = new ApiService();
+            this.LoadSubjects();
+        }
+
+        private async void LoadSubjects()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
