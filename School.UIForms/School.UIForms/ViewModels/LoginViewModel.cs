89f987c74949dd554dd66919429a777b5b5ea09f
Eduardo Fong Dubon <eduardofongdubon@hotmail.com>
2020-05-20 16:46:28 -0500

    Crear proyecto comun para establecer puente entre Web y movil, Funcionalidad de LoginViewModel

--- School.UIForms/School.UIForms/ViewModels/LoginViewModel.cs
+++ School.UIForms/School.UIForms/ViewModels/LoginViewModel.cs
@@ -1,10 +1,52 @@
 ﻿namespace School.UIForms.ViewModels
 {
+	using GalaSoft.MvvmLight.Command;
+	using System;
 	using System.Windows.Input;
-	public class LoginViewModel
+    using Xamarin.Forms;
+
+    public class LoginViewModel
 	{
 		public string Email { get; set; }
 		public string Password { get; set; }
-		public ICommand LoginCommand { get; set; }
+		public ICommand LoginCommand => new RelayCommand(Login);
+
+		private async void Login()
+		{
+			if (string.IsNullOrEmpty(this.Email))
+			{
+				await Application.Current.MainPage.DisplayAlert(
+					"Error",
+					"You must enter an Email",
+					"Accept");
+				return;
+			}
+			if (string.IsNullOrEmpty(this.Password))
+			{
+				await Application.Current.MainPage.DisplayAlert(
+					"Error",
+					"You must enter a Password",
+					"Accept");
+				return;
+			}
+			if(!this.Email.Equals("eduardofong@gmail.com")||!this.Password.Equals("123456"))
+			{
+				await Application.Current.MainPage.DisplayAlert(
+					"Error",
+					"User or Password incorrect",
+					"Accept");
+				return;
+			}
+			await Application.Current.MainPage.DisplayAlert(
+					"LISTOOO",
+					"OK",
+					"Accept");
+			return;
+		}
+		public LoginViewModel()
+		{
+			this.Email = "eduardofong@gmail.com";
+			this.Password = "123456";
+		}
 	}
 }
